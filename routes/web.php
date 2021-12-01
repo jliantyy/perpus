bd6ad05c37cc92a3735ad64f8927b0235b569235
jliantyy <nandajulianti117@gmail.com>
2021-12-01 01:07:59 -0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/uji-coba', 'test\UjicobaController@index');
+Route::post('/uji-coba/store', 'test\UjicobaController@store')
+->name('uji-coba.store');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
